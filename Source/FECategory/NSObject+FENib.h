870ddc9257df0d9cf31619c2c043c54ee9928e23
zzzfly121 <zzzfly121@gmail.com>
2014-04-14 00:05:40 +0800

    add resource files

---
+++ Source/FECategory/NSObject+FENib.h
@@ -0,0 +1,14 @@
+//
+//  NSObject+FENib.h
+//  FETestCategory
+//
+//  Created by lv on 13-9-11.
+//  Copyright (c) 2013年 Ferrari. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (FENib)
++ (id)instanceWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)bundleOrNil owner:(id)owner;
++ (id)instanceWithNibName1:(NSString *)nibNameOrNil bundle:(NSBundle *)bundleOrNil owner:(id)owner;
+@end
