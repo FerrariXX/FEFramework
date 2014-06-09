58b231619bd29deede4c0051074264b29ffae1af
zzzfly121 <zzzfly121@gmail.com>
2014-06-09 22:39:18 +0800

    add files

---
+++ Source/FECommonUI/FEToastView.h
@@ -0,0 +1,14 @@
+//
+//  FEToastView.h
+//  xxx
+//
+//  Created by xxx on
+//  Copyright (c) 2012 XXX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FEToastView : UIView
++ (void)showWithTitle:(NSString*)title animation:(BOOL)animation;
++ (void)dismissWithAnimation:(BOOL)animation;
+@end
