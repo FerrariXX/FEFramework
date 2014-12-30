a8c65f2bbb7848fc89b36665dae5b313612b6ac7
zzzfly121 <zzzfly121>
2014-12-31 00:02:35 +0800

    update

--- Source/FETabbarViewController/FETabbarConst.h
+++ Source/FETabbarViewController/FETabbarConst.h
@@ -10,5 +10,5 @@
 
 #define FE_BRIDGE(x)          ((__bridge void*)x)
 #define kStatusOffset         (([[[UIDevice currentDevice] systemVersion] floatValue] > 6.1)? 20 : 0) //ios7.0适配
-#define FERGB(r,g,b,a)        [UIColor colorWithRed:r/225.0 green:g/225.0  blue:b/225.0  alpha:a];
+#define FERGB(r,g,b,a)        [UIColor colorWithRed:r/225.0 green:g/225.0  blue:b/225.0  alpha:a]
 //#define FERGBHex(r,g,b,a)     [UIColor colorWithRed:r/225.0 green:g/225.0  blue:b/225.0  alpha:a/225.0];
